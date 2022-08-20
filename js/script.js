a8e83fd6b74ec8692740ce5682679c78261eb6be
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-08-20 16:58:07 +0200

    Founr error Last comp choise was scossors insted of scIssors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-
-
-
 function getComputerChoise(){
     let computerChoise = ''
     computerChoise = Math.floor(Math.random() * 3) + 1
@@ -10,8 +7,37 @@ function getComputerChoise(){
     }else if(computerChoise === 2){
         return 'Paper'
     }else{
-        return 'Scossors'
+        return 'Scissors'
     }
 }
-getComputerChoise()
-console.log(getComputerChoise())
+let computerSelection = getComputerChoise()
+
+console.log(`Comp selection is ${computerSelection}`)
+
+function playGame(playerSelection, computerSelection){
+
+    if(playerSelection === computerSelection){
+        console.log("No one wins") 
+    }
+    // Rock
+    if(playerSelection === 'Rock' && computerSelection === 'Paper'){
+        console.log("You luse Paper wins")
+    } 
+    if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
+        console.log("You win Rock wins")
+    }
+    if(playerSelection === 'Paper' && computerSelection === 'Rock'){
+        console.log("You win Paper wins")
+    }
+    if(playerSelection === 'Paper' && computerSelection === 'Scissors'){
+        console.log("You luse Scissors wins")
+    }
+    if(playerSelection === 'Scissors' && computerSelection === 'Rock'){
+        console.log("You luse Rock wins")
+    }
+    if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
+        console.log("You win Scissors wins")
+    }
+    console.log(`Player selection is ${playerSelection}`)
+}
+playGame( "Rock", computerSelection)
